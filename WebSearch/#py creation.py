2e30fb26ac2c59166a1f62dcbc874ce9a83cf76a
Themadhood's Codes <148127209+Themadhood@users.noreply.github.com>
2024-03-26 15:09:33 -0500

    Add files via upload

---
+++ WebSearch/#py creation.py
@@ -0,0 +1,49 @@
+__Program__     = "#py creation"    
+__programer__   = "Themadhood Pequot"
+__Date__        = "3/22/2024"
+__version__     = "0.0.1"
+__update__      = ""
+__info__        = ""   
+
+import Error
+
+st = Error.time.time()
+Error.LenTime(st)
+Error.Log(f"","Log.txt")
+
+try:
+    pass
+
+except Exception as e:
+    if self._Error:
+        raise
+    Error.UploadError([__Program__,__version__,"class","def",
+                                f"mesage",e],"error sheet")
+
+
+
+
+["Small Apps","ThemadhoodsGoodsWorkApp","GameFoundations","Evilution Game",
+ "GoogleSheet","GameFoundationPresets","Login","Functions","CookingApp"]
+
+
+
+
+if __name__ == "__main__":
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
